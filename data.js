2a6dcfae067f1fd8d9ee1ba901f3c925c23ba018
Sanuda Sandipa <sanudasandipa29@gmail.com>
2024-07-06 17:37:05 +0530

    first

---
+++ data.js
@@ -0,0 +1,45 @@
+let songs = [
+
+    {
+        name: 'Without Me',
+        path:  'musics/Halsey - Without Me.mp3',
+        artist: 'Halsey',
+        cover:  'images/Without-Me.jpg'
+    },
+
+    {
+        name: 'RIDE IT',
+        path:  'musics/JAY SEAN - RIDE IT.mp3',
+        artist: 'JAY SEAN ',
+        cover:  'images/rideit.png'
+    },
+
+    {
+        name: 'Let Me Down Slowly',
+        path:  'musics/Alec Benjamin - Let Me Down Slowly.mp3',
+        artist: 'Alec Benjamin',
+        cover:  'images/Without-Me.jpg'
+    },
+
+
+    {
+        name: 'New Rules',
+        path:  'musics/Dua Lipa - New Rules.mp3',
+        artist: 'Dua Lipa',
+        cover:  'images/newrules.jpg'
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
+
